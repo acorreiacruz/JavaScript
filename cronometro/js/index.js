e1d9c48b1ad342f208a1281dd557d944c28497bf
Antonio Correia <antoniocorreiacruz@outlook.com>
2022-01-18 17:44:12 -0300

    Atualizando o CSS e o JS
    Adicionando novos estilos ao CSS e modificando o JS.

--- cronometro/js/index.js
+++ cronometro/js/index.js
@@ -1,3 +1,50 @@
-const btnIniciar = document.querySelector('.btn-parar');
-const btnParar = document.querySelector('.btn-parar');
-const btnZerar = document.querySelector('.btn-parar');
+const cronometro = document.querySelector('.cronometro');
+
+let t = 0;
+let idTimer;
+
+function limparTimer(){
+    cronometro.innerHTML = '';
+}
+
+function adicionaTimer(){
+     const hora = incrementaTimer();
+     cronometro.innerHTML = hora;
+}
+
+function incrementaTimer(){
+    const hora = timer(t);
+    t += 1000;
+    return hora; 
+}
+
+function timer(t) {
+    const i = 3600 * 3 * 1000;
+    const data = new Date(i + t);
+    return data.toLocaleString("pt-BR",{
+        hour12: false,
+        hourCycle: "h24",
+        timeStyle: "medium",
+    });
+
+}
+
+function inciarCronometro() {
+    
+}
+
+document.addEventListener('click',(event) => {
+    const el = event.target;
+
+    if(el.classList.contains('btn-iniciar')){
+        idTimer = setInterval(incrementaTimer,1000);
+    }
+
+    if(el.classList.contains('btn-parar')){
+        console.log('Parar!');
+    }
+
+    if(el.classList.contains('btn-zerar')){
+        console.log('Zerar!');
+    }
+});

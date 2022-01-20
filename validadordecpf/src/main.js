9c22712b2f7e40bf6aa1f224f69e78ec286c3b9e
Antonio Correia <antoniocorreiacruz@outlook.com>
2022-01-19 21:08:49 -0300

    Organização Inicial
    Esqueleto inicial completo dos arquivos como CSS,HTML, WebPack, JS.

---
+++ validadordecpf/src/main.js
@@ -0,0 +1,6 @@
+// Para navegadores muito antigos , é bom fazer essas importaçõs previne erros
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+// Importações normais
+import './assets/css/style.css';
